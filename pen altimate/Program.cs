f83608c2a1fd60d62f319071f20a753b6e315f70
Your Name <your@example.com>
2016-05-06 14:31:59 -0400

    PenAltimate_Initial

---
+++ pen altimate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pen_altimate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "This is for Code Eval";
+            string[] inputWords = line.Split();
+            int lenOfArray = inputWords.Length;
+            Console.WriteLine(inputWords[lenOfArray -2] ); 
+
+        }
+    }
+}
